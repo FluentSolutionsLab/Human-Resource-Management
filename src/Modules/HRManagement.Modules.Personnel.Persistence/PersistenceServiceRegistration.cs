f18418fb557d37489c15fd73e8251921e29f1a31
Gabin Dongmo <gdongmo@gmail.com>
2023-04-11 21:46:50 -0400

    Better encapsulation

--- src/Modules/HRManagement.Modules.Personnel.Persistence/PersistenceServiceRegistration.cs
+++ src/Modules/HRManagement.Modules.Personnel.Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using HRManagement.Modules.Personnel.Application;
+﻿using System;
+using System.Threading.Tasks;
+using HRManagement.Modules.Personnel.Application;
 using HRManagement.Modules.Personnel.Application.Contracts;
 using HRManagement.Modules.Personnel.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,10 +9,19 @@ namespace HRManagement.Modules.Personnel.Persistence;
 
 public static class PersistenceServiceRegistration
 {
-    public static void AddPersistenceServices(this IServiceCollection services)
+    public static void AddModulePersonnelManagement(this IServiceCollection services)
     {
         services.AddApplicationServices();
         services.AddDbContext<PersonnelDbContext>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
+    
+    public static async Task ModulePersonnelManagementDatabaseInitializer(this IServiceProvider provider)
+    {
+        using var serviceScope = provider.CreateScope();
+        var services = serviceScope.ServiceProvider;
+        var referenceDbContext = services.GetRequiredService<PersonnelDbContext>();
+        await DatabaseInitializer.InitializeAsync(referenceDbContext);
+    }
+
 }
