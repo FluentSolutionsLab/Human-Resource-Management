47e25e9acae87a624505ee5ab35a728aaf81a44c
Gabin Dongmo <gdongmo@gmail.com>
2023-04-29 22:07:26 -0400

    Adding memory cache support for query GetEmployeesList

--- src/Modules/HRManagement.Modules.Personnel.Application/UseCases/Employee/Find/GetEmployeesQueryHandler.cs
+++ src/Modules/HRManagement.Modules.Personnel.Application/UseCases/Employee/Find/GetEmployeesQueryHandler.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Text;
 using HRManagement.Modules.Personnel.Application.Contracts;
 using HRManagement.Modules.Personnel.Domain;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace HRManagement.Modules.Personnel.Application.UseCases;
 
 public class GetEmployeesQueryHandler : IQueryHandler<GetEmployeesQuery, Result<PagedList<EmployeeDto>>>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMemoryCache _cache;
 
-    public GetEmployeesQueryHandler(IUnitOfWork unitOfWork)
+    public GetEmployeesQueryHandler(IUnitOfWork unitOfWork, IMemoryCache cache)
     {
         _unitOfWork = unitOfWork;
+        _cache = cache;
     }
 
     public async Task<Result<PagedList<EmployeeDto>>> Handle(GetEmployeesQuery request, CancellationToken cancellationToken)
@@ -19,6 +23,9 @@ public class GetEmployeesQueryHandler : IQueryHandler<GetEmployeesQuery, Result<
         var pageNumber = request.FilterParameters.PageNumber;
         var pageSize = request.FilterParameters.PageSize;
 
+        var sb = new StringBuilder();
+        sb.Append($"employeeList?pageNumber={pageNumber}&pageSize={pageSize}");
+        
         Expression<Func<Employee, bool>> filter = null;
         if (!string.IsNullOrWhiteSpace(request.FilterParameters.SearchQuery))
         {
@@ -27,12 +34,24 @@ public class GetEmployeesQueryHandler : IQueryHandler<GetEmployeesQuery, Result<
                                  || employee.Name.LastName.Contains(searchQuery)
                                  || employee.EmailAddress.Email.Contains(searchQuery)
                                  || employee.Role.Name.Contains(searchQuery);
+            sb.Append($"&searchQuery={searchQuery}");
         }
 
-        var employees = await _unitOfWork.Employees.GetAsync(
-            filter: filter,
-            pageNumber: pageNumber,
-            pageSize: pageSize);
+        var employeeListCacheKey = sb.ToString();
+        if (!_cache.TryGetValue(employeeListCacheKey, out PagedList<Employee> employees))
+        {
+            employees = await _unitOfWork.Employees.GetAsync(
+                filter: filter,
+                pageNumber: pageNumber,
+                pageSize: pageSize);
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(3600))
+                .SetPriority(CacheItemPriority.Normal)
+                .SetSize(1024);
+            _cache.Set(employeeListCacheKey, employees, cacheEntryOptions);
+        }
 
         var dtos = employees.Select(x => x.ToResponseDto()).ToList();
 
