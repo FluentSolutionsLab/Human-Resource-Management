b42b38865e054192a7f2f3b4ff78ad38675a9986
Gabin Dongmo <gdongmo@gmail.com>
2023-04-30 01:48:15 -0400

    Cache query on single employee

--- src/Modules/HRManagement.Modules.Personnel.Application/UseCases/Employee/Find/GetEmployeeQueryHandler.cs
+++ src/Modules/HRManagement.Modules.Personnel.Application/UseCases/Employee/Find/GetEmployeeQueryHandler.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using HRManagement.Modules.Personnel.Application.Contracts;
 using HRManagement.Modules.Personnel.Domain;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace HRManagement.Modules.Personnel.Application.UseCases;
 
 public class GetEmployeeQueryHandler : IQueryHandler<GetEmployeeQuery, Result<EmployeeExtendedDto, Error>>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMemoryCache _cache;
 
-    public GetEmployeeQueryHandler(IUnitOfWork unitOfWork)
+    public GetEmployeeQueryHandler(IUnitOfWork unitOfWork, IMemoryCache cache)
     {
         _unitOfWork = unitOfWork;
+        _cache = cache;
     }
 
     public async Task<Result<EmployeeExtendedDto, Error>> Handle(GetEmployeeQuery request, CancellationToken cancellationToken)
@@ -18,10 +21,21 @@ public class GetEmployeeQueryHandler : IQueryHandler<GetEmployeeQuery, Result<Em
         if (!Guid.TryParse(request.EmployeeId, out var employeeId))
             return DomainErrors.NotFound(nameof(Employee), request.EmployeeId);
 
-        Maybe<Employee> employee = await _unitOfWork.Employees.GetByIdAsync(employeeId);
-        if (employee.HasNoValue)
-            return DomainErrors.NotFound(nameof(Employee), request.EmployeeId);
+        var queryCacheKey = $"GetEmployeeQuery/{employeeId}";
+        if (!_cache.TryGetValue(queryCacheKey, out Employee employee))
+        {
+            employee = await _unitOfWork.Employees.GetByIdAsync(employeeId);
+            if (employee == null)
+                return DomainErrors.NotFound(nameof(Employee), request.EmployeeId);
 
-        return employee.Value.ToResponseExtendedDto();
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(3600))
+                .SetPriority(CacheItemPriority.Normal)
+                .SetSize(1024);
+            _cache.Set(queryCacheKey, employee, cacheEntryOptions);
+        }
+        
+        return employee.ToResponseExtendedDto();
     }
 }
