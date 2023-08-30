23fb595f243e79bd2a328eccfda87b098e566652
Gabin Dongmo <gdongmo@gmail.com>
2023-08-29 22:00:42 -0400

    Improving unit tests: adding builders

---
+++ src/Tests/HRManagement.Personnel.Application.UnitTests/Builders/EmployeeBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace HRManagement.Personnel.Application.UnitTests.Builders;
+
+public class EmployeeBuilder
+{
+    private readonly Person _fakePerson = new Faker().Person;
+    private Employee _employee = null;
+
+    public EmployeeBuilder WithFixture()
+    {
+        var hiringDate = new Faker().Date.Past(15);
+        _employee = Employee.Create(
+            Name.Create(_fakePerson.FirstName, _fakePerson.LastName).Value,
+            EmailAddress.Create(_fakePerson.Email).Value,
+            ValueDate.Create(_fakePerson.DateOfBirth.ToString("d")).Value,
+            ValueDate.Create(hiringDate.ToString("d")).Value,
+            Role.Create("ceo", null).Value,
+            null).Value;
+        return this;
+    }
+
+    public Employee Build() => _employee;
+}
---
+++ src/Tests/HRManagement.Personnel.Application.UnitTests/Builders/HireEmployeeCommandBuilder.cs
@@ -0,0 +1,55 @@
+﻿namespace HRManagement.Personnel.Application.UnitTests.Builders;
+
+public class HireEmployeeCommandBuilder
+{
+    private readonly Person _fakePerson = new Faker().Person;
+    private readonly HireEmployeeCommand _command = new();
+
+    public HireEmployeeCommandBuilder WithEmailAddress(string email)
+    {
+        _command.EmailAddress = email;
+        return this;
+    }
+
+    public HireEmployeeCommandBuilder WithFirstName(string firstName)
+    {
+        _command.FirstName = firstName;
+        return this;
+    }
+
+    public HireEmployeeCommandBuilder WithLastName(string lastName)
+    {
+        _command.LastName = lastName;
+        return this;
+    }
+
+    public HireEmployeeCommandBuilder WithDateOfBirth(string dateOfBirth)
+    {
+        _command.DateOfBirth = dateOfBirth;
+        return this;
+    }
+
+    public HireEmployeeCommandBuilder WithManagerId(string reportsToId)
+    {
+        _command.ReportsToId = reportsToId;
+        return this;
+    }
+
+    public HireEmployeeCommandBuilder WithFixture()
+    {
+        _command.EmailAddress = _fakePerson.Email;
+        _command.FirstName = _fakePerson.FirstName;
+        _command.LastName = _fakePerson.LastName;
+        _command.DateOfBirth = _fakePerson.DateOfBirth.Date.ToString("d");
+        _command.HiringDate = new Faker().Date.Recent(60).ToString("d");
+        _command.ReportsToId = It.IsNotNull<Guid>().ToString();
+        _command.RoleId = It.IsAny<byte>();
+
+        return this;
+    }
+
+    public HireEmployeeCommand Build()
+    {
+        return _command;
+    }
+}
