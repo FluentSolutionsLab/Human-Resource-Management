743c336fbdc7ba11e64e08c113da85683f96a4cd
Gabin Dongmo <gdongmo@gmail.com>
2023-11-26 23:38:54 -0500

    Add pagination to role query + refactoring

--- src/Modules/HRManagement.Modules.Personnel.Application/UseCases/FindRoles/GetRoleByIdQueryHandler.cs
+++ src/Modules/HRManagement.Modules.Personnel.Application/UseCases/FindRoles/GetRoleByIdQueryHandler.cs
@@ -5,17 +5,27 @@ namespace HRManagement.Modules.Personnel.Application.UseCases;
 public class GetRoleByIdQueryHandler : IQueryHandler<GetRoleByIdQuery, Result<RoleDto, Error>>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ICacheService _cacheService;
 
-    public GetRoleByIdQueryHandler(IUnitOfWork unitOfWork)
+    public GetRoleByIdQueryHandler(IUnitOfWork unitOfWork, ICacheService cacheService)
     {
         _unitOfWork = unitOfWork;
+        _cacheService = cacheService;
     }
 
     public async Task<Result<RoleDto, Error>> Handle(GetRoleByIdQuery request, CancellationToken cancellationToken)
     {
-        var role = await _unitOfWork.GetRepository<Role, byte>().GetByIdAsync(request.Id);
-        if (role == null) return DomainErrors.NotFound(nameof(Role), request.Id);
+        var queryCacheKey = $"GetRoleQuery/{request.Id}";
+        var roleOrNothing = _cacheService.Get<Maybe<Role>>(queryCacheKey);
+        if (roleOrNothing.HasNoValue)
+        {
+            roleOrNothing = await _unitOfWork.GetRepository<Role, byte>().GetByIdAsync(request.Id, "ReportsTo");
+            if (roleOrNothing.HasValue)
+                _cacheService.Set(queryCacheKey, roleOrNothing);
+            else
+                return DomainErrors.NotFound(nameof(Role), request.Id);
+        }
 
-        return role.Value.ToResponseDto();
+        return roleOrNothing.Value.ToResponseDto();
     }
 }
